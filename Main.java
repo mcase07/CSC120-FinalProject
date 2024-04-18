76b3c5dfe0e19b3bfc9175374862473bbc2fb167
Mack Case <hcase@smith.edu>
2024-04-18 12:06:00 -0400

    player methods and main

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("\nWelcome to Wellness World. This is a shopping center that you can explore! Make purchases, get services done, and enjoy!\n");
+        System.out.println("The Hair Salon is to the north.");
+        System.out.println("The Nail Salon is to the south.");
+        System.out.println("The Book Store is to the east.");
+        System.out.println("The Cafe is to the west.");
+        System.out.println("\nEnter a direction to move and start exploring!");
+
+        Player Player1 = new Player("Jo");
+        // Player1.move();
+        //call move when they choose 
+    }
+    
+}
--- Player.java
+++ Player.java
@@ -1,6 +1,40 @@
 public class Player {
     
     String name;
-    int age;
+    String position;
 
+    public Player(String name){
+
+        this.name = name;
+        this.position = "Home";
+
+        System.out.println("Hello, " + this.name + "! Currently, you are at " + position + ".");
+
+    }
+    
+    String move(String direction){
+
+        if (direction == "NORTH"){
+            this.position = "the Hair Salon";
+            return "You are at " + position;
+        } else if (direction == "SOUTH"){
+            this.position = "the Nail Salon";
+            return "You are at " + position;
+        }else if (direction == "EAST"){
+            this.position = "the Book Store";
+            return "You are at " + position;
+        }else if (direction == "WEST"){
+            this.position = "the Cafe";
+            return "You are at " + position;
+        }else{
+            return "That's not a valid direction, choose again!";
+        }
+    }
+
+    public static void main(String[] args) {
+        Player Player1 = new Player("Jo");
+
+        // will do userResponse = userInput.nextLine.toUpperCase();
+        System.out.println(Player1.move("EAST"));
+    }
 }
