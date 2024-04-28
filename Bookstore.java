d05d6e6d34c3f920a00c9656fca1e043916cbfd4
Joberlye <jhernandez02@smith.edu>
2024-04-28 16:36:44 -0400

    sit and getUp

--- Bookstore.java
+++ Bookstore.java
@@ -1,64 +1,67 @@
 import java.util.ArrayList;
-import java.util.Hashtable;
 
 public class Bookstore {
     ArrayList<String> menu = new ArrayList<String>();
-    private Hashtable<String, Boolean> books; //change to be called inventory
+    private ArrayList<String> books; //change to be called inventory
+    public Boolean seat = false;
+
 
 public Bookstore (){
-    books.put("Parable of the Sower by Octavia Butler" , true);
-    books.put("Kindred by Octavia Butler", true);
-    books.put("Bloodchild and Other Stories by Octavia Butler", true);
-    books.put("Dawn by Octavia Butler", true);
-    books.put("Patternmaster by Octavia Butler", true);
-    books.put("Imago by Octavia Butler", true);
-    books.put("True to the Game by Teri Woods", true);
-    books.put("Dutch III by Teri Woods", true);
-    books.put("Dutch by Teri Woods", true);
-    books.put("Alibi by Teri Woods", true);
-    books.put("In the Meantime by Iyanla Vanzant", true);
-    books.put("Acts Of Faith: Meditations For People of Color by Iyanla Vanzant", true);
-    books.put("The Spirit of a Man by Iyanla Vanzant", true);
-    books.put("Black Economics by Jawanza Kunjufu", true);
-    books.put("Educating Black Girls by Jawanza Kunjufu", true);
+    books.add("Parable of the Sower by Octavia Butler");
+    books.add("Kindred by Octavia Butler");
+    books.add("Bloodchild and Other Stories by Octavia Butler");
+    books.add("Dawn by Octavia Butler");
+    books.add("Patternmaster by Octavia Butler");
+    books.add("Imago by Octavia Butler");
+    books.add("True to the Game by Teri Woods");
+    books.add("Dutch III by Teri Woods");
+    books.add("Dutch by Teri Woods");
+    books.add("Alibi by Teri Woods");
+    books.add("In the Meantime by Iyanla Vanzant");
+    books.add("Acts Of Faith: Meditations For People of Color by Iyanla Vanzant");
+    books.add("The Spirit of a Man by Iyanla Vanzant");
+    books.add("Black Economics by Jawanza Kunjufu");
+    books.add("Educating Black Girls by Jawanza Kunjufu");
 }
 
-public void menu(){ //this will in main
-    menu.add("Browse");
-    menu.add("Buy");
-    System.out.println( "Hi Welcome to or Bookstore! Here is our menu: " + this.menu);
-}
 
-//get rid of browse
-// Check out a book in 'books'
-public void Browse(String name){
+public void chooseBook(String name){
     if(this.books.contains(name)){ // checking if this is in book collection
-      if(this.books.get(name)){ //gets the book from collection
+      this.books.remove(name); //removes the book from collection
       System.out.println(name + "is availiable to look at"); //prints book
-      }
     }
     // if the 'book' isn't in the collection, prints the string below
     else{
-      System.out.println(name + "is not available to browse at the moment"); //prints book + ____
- 
-          }
-  }
-
-  public void Buy(String name){ //goodbye this is in player now 
-    if(this.books.contains(name)){ // checking if this is in book collection
-      if(this.books.get(name)){ //gets the book from collection
-      System.out.println(name + "is availiable to buy"); //prints book
-    // pay()
-      }
+      System.out.println(name + "is not available to read at the moment"); //prints book + ____ 
     }
-    // if the 'book' isn't in the collection, prints the string below
-    else{
-      System.out.println(name + "is not available to buy at the moment, try looking for another book."); //prints book + ____
-          }
+}  
+
+public void returnBook(Player player, String name){
+   if(player.bag.contains(name) == true){
+      this.books.add(name);
+   }
+   else {
+    System.out.println("You don't have a book to return.");
+   }
+}
+
+public void sit(Player player){
+ if(this.seat == false){
+    System.out.println("You are standing right now.");
+ }
+ else{
+  if(this.seat == true){
+    System.out.println("You are now sitting down");
+
   }
+ }
+}
 
-  //the only method we have in the class will be 
-  // chooseBook()
+public void getUp(Player player){
+  if(this.seat == false){
+
+  }
+}
 
 
-}  //implement contract last
+}
